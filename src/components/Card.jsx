0e15443562cfa093b6b498badeff0779de741fca
Olzhas Kurikov <okur@hellofresh.com>
2021-03-10 18:00:11 +0100

    update: added delete button for individual trip card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Trash} from 'react-feather'
 
 const Card = ({ city, date }) => {
     return (
@@ -6,6 +7,9 @@ const Card = ({ city, date }) => {
             <img style={{borderRadius: '5px', marginBottom: '5px'}} src="https://via.placeholder.com/150" />
             <h5 class="card-title">{city}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{date}</h6>
+            <button onClick={() => console.log('I will delete this trip')} type="button" class="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <Trash />
+            </button>
         </div>
     )
 }
