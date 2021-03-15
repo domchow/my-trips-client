57fa49db9788de0e228c187d3dbc8cf26010d222
Olzhas Kurikov <okur@hellofresh.com>
2021-03-15 17:40:56 +0100

    update: creating sign-in form

--- src/pages/sign-in/SignIn.tsx
+++ src/pages/sign-in/SignIn.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const ContainerWithImage = styled.div`
+    background-image: url("https://images.unsplash.com/photo-1490676940747-c906c30765d6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80"); /* The image used */
+    background-color: #cccccc; /* Used if the image is unavailable */
+    height: 100vh;
+    width: 100vw;
+    background-position: center; /* Center the image */
+    background-repeat: no-repeat; /* Do not repeat the image */
+    background-size: cover; /* Resize the background image to cover the entire container */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const Card = styled.div`
+    background-color: white;
+    border-radius: 20px;
+    opacity: .95;
+    padding: 15px;
+    width: 500px;
+    height: 700px;
+`
 
 const SignIn = () => {
-    return <h1>Login Page</h1>
+    return (
+        <ContainerWithImage>
+            <Card>login form</Card>
+        </ContainerWithImage>
+    ) 
+
 }
 
 export default SignIn
