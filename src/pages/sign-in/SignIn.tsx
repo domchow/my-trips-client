ebfad0df21fd03ecd62af287dcb0e6974c89ca9e
Olzhas Kurikov <okur@hellofresh.com>
2021-03-16 17:59:44 +0100

    update: removed template svg image

--- src/pages/sign-in/SignIn.tsx
+++ src/pages/sign-in/SignIn.tsx
@@ -34,7 +34,6 @@ const SignIn = () => {
             <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
                 <div className="mx-auto w-full max-w-sm lg:w-96">
                     <div>
-                        <img className="h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
                         <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
                             Sign in to your account
                         </h2>
